9f3e7507a92b34564bf07dbc6514562a50d9eef4
曾子聪(Ifan Zeng) <zengzicong@hisense.ad>
2023-10-17 17:27:41 +0800

    This is my 4th patch

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 using namespace std;
 int main() {
+    cout << "This is my test string 0.5" << endl;
     cout << "This is my test string 01" << endl;
     cout << "This is my test string 1.5" << endl;
     cout << "This is my test string 02" << endl;
