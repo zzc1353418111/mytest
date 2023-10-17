aec5122c9f187ca21fcfa6f620102c3222dbfff6
曾子聪(Ifan Zeng) <zengzicong@hisense.ad>
2023-10-17 16:53:39 +0800

    This is my 1st patch

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+    cout << "This is my test string 01" << endl;
+    cout << "This is my test string 02" << endl;
+    cout << "This is my test string 03" << endl;
+    cout << "This is my test string 04" << endl;
+    cout << "This is my test string 05" << endl;
+}
